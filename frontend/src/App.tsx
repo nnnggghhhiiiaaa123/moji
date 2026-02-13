55c83eaf89d233eda452cbf3ec785fc274e8c512
unknown <oootui123.com>
2026-02-13 13:58:17 +0700

    cài đat frontend voi Vite React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+import SignInPage from './pages/SignInPage'
+import { SignUpPage } from './pages/SignUpPage'
+import ChatAppPage from './pages/ChatAppPage';
+import { Toaster } from 'sonner';
+
+function App() {
+  return <>
+    <Toaster richColors />
+    <BrowserRouter>
+      <Routes>
+
+        {/* public routes */}
+        <Route
+          path='/signin'
+          element={<SignInPage />}
+        />
+        <Route
+          path='/signup'
+          element={<SignUpPage />}
+        />
+        {/* protected routes */}
+        {/* todo: tạo protected route */}
+        <Route
+          path='/'
+          element={<ChatAppPage />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  </>;
+}
+
+export default App;
